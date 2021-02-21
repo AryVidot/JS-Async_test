5222497418677fced6b74086d8929ebefbc2cfe1
Ary Vidot <aryvid@gmail.com>
2021-02-21 22:07:21 +0000

    get and create post function added

--- callbacks.js
+++ callbacks.js
@@ -4,10 +4,25 @@ const posts = [
 ];
 
 // mimicking how to fetch from a server
-function getPOst(){
+function getPost(){
     // delaying a function
-    setTimeout(function(){
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post,index) => {
+            output += `<li>${post.title}</li>`
+        });
+        document.body.innerHTML = output;
 
     }, 1000)
     // 1000 milliseconds = 1second
+
 }
+
+createPost(post) {
+    setTimeout(() => {
+        posts.push(posts);
+    }, 2000);
+}
+
+
+getPost();
