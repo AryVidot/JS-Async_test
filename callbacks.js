b86f24e5d9bebca0eb5e087ee01f7b9cae83142a
Ary Vidot <aryvid@gmail.com>
2021-02-21 21:51:44 +0000

    first commit

---
+++ callbacks.js
@@ -0,0 +1,13 @@
+const posts = [
+    { title: 'Post One', body: 'This is post one'},
+    {title: 'Post Two', body: 'This is post one'},
+];
+
+// mimicking how to fetch from a server
+function getPOst(){
+    // delaying a function
+    setTimeout(function(){
+
+    }, 1000)
+    // 1000 milliseconds = 1second
+}
